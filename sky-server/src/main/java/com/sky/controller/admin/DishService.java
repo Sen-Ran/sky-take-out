8d44730b7cde9c37c8d514962d256956e972b3be
HumphreyB612 <86584350+HumphreyB612@users.noreply.github.com>
2024-12-10 11:06:33 +0800

    新增菜品业务代码开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/DishService.java
@@ -0,0 +1,14 @@
+package com.sky.controller.admin;
+
+
+import com.sky.dto.DishDTO;
+
+public interface DishService {
+
+
+    /**
+     * 新增菜品和对应的口味数据
+     * @param dishDTO
+     */
+    public void saveWithFlavor(DishDTO dishDTO);
+}
